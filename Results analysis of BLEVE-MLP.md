a00815c6df0711a4c83cf8e42b68695bffadf2d0
qilin <kylnlovesummer@gmail.com>
2020-09-15 21:07:41 +0800

    Added mish, SGD, Ranger

---
+++ Results analysis of BLEVE-MLP.md
@@ -0,0 +1,38 @@
+## Results analysis of BLEVE-MLP
+
+### Hidden = 1
+
+- Training: p↓ train_loss↓
+- p=0.2 is the best
+- Batch size seems not a big problem
+- batch size 512 seems best for val
+- p↑ seems better for mape_test
+- Best_mape_val top 5: batch=2048_p=0.2 (10.1%), batch=128_p=0.2, batch=256_p=0.3,batch=512_p=0.2,batch=512,p=0.1(10.87%)
+- mape_test top 5: batch=1024_p=0.3 (10%),batch=512_p=0.3, batch=1024,p=0.1, batch=256_p=0.3,batch=512_p=0.2(10.88%)
+
+## Hidden=2
+
+- p↓ and neurons↑ can get better training loss
+- batch size 512 seems best for val
+- p=0 can get very good results but also potentially lead to bad val and test results (overfitting)
+- Best_mape_val: top 5: n256_b128_p0.1(10.8%), n=128_b=1024_p=0.1, n256_b512_p0.1, n256_b512_p0(11.35%)
+- Mape_test top 5: n256_b128_p0.1 (8.97%), n=128_b256_p0.4, n256_b2048_p0.1, n256_b512_p0.1(9.56%)
+
+## Hidden = 3
+
+- p↓ and neurons↑ can get better training loss
+- batch512 still good, 
+- p↓ good for val_loss, p=0, 0.1 is better for  val and test
+- Best_mape_val: top 5: n256_b128_p0(11.27%), n256_b1024_p0.1, n256_b256_p0.1, n128_b1024_p0.1, n256_b512_p0.1(12.62%)
+- Mape_test top 5: n256_b512_p0.1(10%), n256_b128_p0(10%), n256_b256_p0, n256_b1024_p0.1, n128_b256_p0(10.4%)
+
+
+
+## Conclusion
+
+Hidden=2, neurons=256, batchSize=512, p=0.1
+
+SGD is better with mom=0.99
+
+
+
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import numpy as np
 import scipy.io as sio
 import math
 from torch import sigmoid, tanh, relu
+from torch.utils.tensorboard import SummaryWriter
+from mish import Mish
+from ranger import Ranger
 
 
 class BLEVEDataset(Dataset):
@@ -21,39 +24,65 @@ class BLEVEDataset(Dataset):
 
 
 class MLPNet(nn.Module):
-    def __init__(self):
+    def __init__(self, features=[10, 256, 256], bn=False, activation_fn='mish', p=0):
         super().__init__()
-        self.fc1 = nn.Linear(10, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.out = nn.Linear(256, 1)
-        self.dropout = nn.Dropout(0.1)
+        self.net = nn.Sequential()
+        if activation_fn == 'sigmoid':
+            self.activation_fn = nn.Sigmoid()
+        elif activation_fn == 'tanh':
+            self.activation_fn = nn.Tanh()
+        elif activation_fn == 'mish':
+            self.activation_fn = Mish()
+        else:
+            self.activation_fn = nn.ReLU(inplace=True)
+
+        self.features = features
+        self.bn = bn
+        self.activation_fn_name = activation_fn
+        self.p = p
+
+        for layer in range(1, len(features)):
+            self.net.add_module('fc%d' % layer, nn.Linear(features[layer - 1], features[layer]))
+            if bn:
+                self.net.add_module('bn%d' % layer, nn.BatchNorm1d(features[layer]))
+            self.net.add_module('sig%d' % layer, self.activation_fn)
+            if p > 0:
+                self.net.add_module('dp%d' % layer, nn.Dropout(p))
+        self.net.add_module('out', nn.Linear(features[-1], 1))
 
         for m in self.modules():
             if isinstance(m, nn.Linear):
                 m.weight.data = nn.init.xavier_normal_(m.weight.data)
+                m.bias.data = torch.nn.init.zeros_(m.bias.data)
+            if isinstance(m, nn.BatchNorm1d):
+                m.weight.data = nn.init.normal_(m.weight.data, mean=1, std=0.02)
+                m.bias.data = torch.nn.init.zeros_(m.bias.data)
 
     def forward(self, x):
-        x = sigmoid(self.fc1(x))
-        x = sigmoid(self.fc2(x))
-        x = self.out(x)
+        x = self.net(x)
         return x
-
-
-def weights_init(m):
-    if isinstance(m, nn.Linear):
-        nn.init.xavier_normal_(m)
-    if isinstance(m, nn.BatchNorm):
-        nn.init.xavier_normal_(m)
 
 
 def mean_absolute_percentage_error(y_true, y_pred):
     return torch.mean(torch.abs(y_true - y_pred) / y_true) * 100
 
 
-def train(model, dataset, val_X, val_y, batch_size=512, epochs=1000, epoch_show=1):
+def train(model, dataset, val_X, val_y, batch_size=512, epochs=500, epoch_show=1, weight_decay=1e-5, momentum=0.99):
     train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
-    optimizer = optim.Adam(model.parameters())
+    # optimizer = Ranger(model.parameters(), weight_decay=weight_decay)
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=momentum, weight_decay=weight_decay)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                     mode='min',
+                                                     factor=0.1,
+                                                     patience=50,
+                                                     threshold=0,
+                                                     verbose=True)
     loss_fn = nn.MSELoss()
+
+    writer = SummaryWriter('runs/Mish/hidden={}_neurons={}_{}_batch={:04d}_bn={}_p={:.1f}_mom={}_l2={}'.format(
+        len(model.features)-1, model.features[-1], model.activation_fn_name, batch_size, model.bn, model.p, momentum,
+        weight_decay
+    ))
 
     best_val_mape = 100
     for epoch in range(epochs):
@@ -62,37 +91,44 @@ def train(model, dataset, val_X, val_y, batch_size=512, epochs=1000, epoch_show=
         for i, data in enumerate(train_loader, 0):
             x, y = data
             out = model(x)
-            loss_iter = loss_fn(out.squeeze(), y)
+            loss_iter = loss_fn(y, out.squeeze())
             optimizer.zero_grad()
             loss_iter.backward()
             optimizer.step()
-            loss_epoch += loss_iter / batch_size
+            loss_epoch += loss_iter
         if epoch % epoch_show == 0 or epoch == epochs - 1:
             with torch.no_grad():
                 model.eval()
                 pred = model(val_X)
-                loss_val = loss_fn(pred.squeeze(), val_y)
+                loss_val = loss_fn(val_y, pred.squeeze())
                 mape = mean_absolute_percentage_error(val_y, pred.squeeze())
-                print('\nEpoch {:03d}: loss_train={:.6f}, loss_val={:.6f}, val_mape={:.4f}'.format(
-                    epoch, loss_epoch, loss_val, mape), end='  ')
+                scheduler.step(best_val_mape)
+                print('\nEpoch {:03d}: loss_train={:.6f}, loss_val={:.6f}, val_mape={:.4f}, '
+                      'best_val_mape={:.4f}'.format(epoch, loss_epoch, loss_val, mape, best_val_mape), end='  ')
                 if mape < best_val_mape:
                     model_name = 'best_model.pt'
                     print('Val_mape improved from {:.4f} to {:.4f}, saving model to {}'.format(
                         best_val_mape, mape, model_name), end=' ')
                     best_val_mape = mape
-                    torch.save(model.state_dict(), 'models\\' + model_name)
+                    torch.save(model.state_dict(), 'models/' + model_name)
+        writer.add_scalar("loss/train", loss_epoch, epoch)
+        writer.add_scalar("loss/val", loss_val, epoch)
+        writer.add_scalar("mape/val", mape, epoch)
+    writer.add_scalar("mape/best_val", best_val_mape)
+    return writer
 
 
-def test(model, models_dir, test_X, test_y):
+def test(writer, model, models_dir, test_X, test_y):
+    model.eval()
     import glob
     models_name = glob.glob(models_dir + '*.pt')
     models_name.sort()
-    model.load_state_dict(torch.load(models_name[-1]))
-    model.eval()
+    model.load_state_dict(torch.load(models_name[-1]), strict=False)
     pred = model(test_X)
     loss = nn.MSELoss()(pred.squeeze(), test_y)
     mape = mean_absolute_percentage_error(test_y, pred.squeeze())
     print('\nloss_test: {:.6f}, mape_test:{:.4f}'.format(loss, mape))
+    writer.add_scalar('mape/test', mape)
 
 
 def load_data(file, device):
@@ -119,7 +155,27 @@ def load_data(file, device):
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     dataset, val_X, val_y, test_X, test_y = load_data(file='BLEVE_simulated_open.npz', device=device)
-    model = MLPNet()
-    model.to(device)
-    train(model, dataset, val_X, val_y)
-    test(model, 'models\\', test_X, test_y)
+
+    activation_list = ['mish']
+    bn_list = [0]
+    p_list = [0, 0.1]
+    batchSize_list = [256, 512, 1024]
+    feature_list = [[10, 256, 256], [10, 256, 256, 256]]
+    momentum_list = [0.99]
+    weight_decay_list = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4]
+
+    for activation_fn in activation_list:
+        for bn in bn_list:
+            for p in p_list:
+                for batch_size in batchSize_list:
+                    for features in feature_list:
+                        for momentum in momentum_list:
+                            for weight_decay in weight_decay_list:
+                                print("{}_bn={}_p={}_batchSize={}_feature={}_mom={}_l2={}".format(
+                                    activation_fn, bn, p, batch_size, features, momentum, weight_decay
+                                ))
+                                model = MLPNet(features=features, activation_fn=activation_fn, bn=bn, p=p)
+                                model.to(device)
+                                writer = train(model, dataset, val_X, val_y, batch_size=batch_size,
+                                               momentum=momentum, weight_decay=weight_decay)
+                                test(writer, model, 'models/', test_X, test_y)
